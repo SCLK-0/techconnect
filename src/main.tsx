8b4d59148012f17056e962e78795110af2e6fb3f
SCLK-0 <it.rbfremorin@gmail.com>
2025-11-14 02:39:47 +0800

    Add developer console Easter egg message

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./components/ThemeProvider";
+import { displayConsoleMessage } from "./utils/consoleMessage";
+
+// Display console message for curious developers
+displayConsoleMessage();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
